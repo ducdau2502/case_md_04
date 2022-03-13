91e23d364d62344e1a98405b4209358a548c959a
ducdau2502 <ducdau2502@gmail.com>
2022-03-13 11:13:03 +0700

    add init

---
+++ src/main/java/get/high/HighApplication.java
@@ -0,0 +1,13 @@
+package get.high;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighApplication.class, args);
+    }
+
+}
---
+++ src/test/java/get/high/HighApplicationTests.java
@@ -0,0 +1,13 @@
+package get.high;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
