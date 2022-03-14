7fd339990aaa88e61945bec3e680d11ebdcc7d49
ducdau2502 <ducdau2502@gmail.com>
2022-03-14 09:21:09 +0700

    add init 2

--- src/main/java/get/high/repository/IGroupsRepository.java
+++ src/main/java/get/high/repository/IGroupsRepository.java
@@ -1,9 +1,9 @@
 package get.high.repository;
 
 import get.high.model.entity.Groups;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IGroupsRepository extends CrudRepository<Groups, Long> {
+public interface IGroupsRepository extends JpaRepository<Groups, Long> {
 }
--- src/main/java/get/high/repository/IUserInforRepository.java
+++ src/main/java/get/high/repository/IUserInforRepository.java
@@ -1,9 +1,9 @@
 package get.high.repository;
 
 import get.high.model.entity.UserInfor;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IUserInforRepository extends CrudRepository<UserInfor, Long> {
+public interface IUserInforRepository extends JpaRepository<UserInfor, Long> {
 }
