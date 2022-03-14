05a81b9016d832eb1afa3a55dd45a609fb4edac4
TienNguyen0612 <sales08.taybactravel@gmail.com>
2022-03-14 16:00:49 +0700

    sorry

--- src/main/java/get/high/repository/ILikeCommentRepository.java
+++ src/main/java/get/high/repository/ILikeCommentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ILikeCommentReposiroty extends JpaRepository<LikeComment, Long> {
+public interface ILikeCommentRepository extends JpaRepository<LikeComment, Long> {
 }
