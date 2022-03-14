067988bce583fa187920c62cd174810c15f198d2
ducdau2502 <ducdau2502@gmail.com>
2022-03-14 09:42:28 +0700

    add

--- src/main/java/get/high/controller/AccountController.java
+++ src/main/java/get/high/controller/AccountController.java
@@ -8,6 +8,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/account")
 public class AccountController {
-    @Autowired
-    private IAccountService accountService;
 }
