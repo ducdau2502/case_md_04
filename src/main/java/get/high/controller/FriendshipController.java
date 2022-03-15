d71ecf8ec34bebf7c6c381e4308d07f0160929a6
ducdau2502 <ducdau2502@gmail.com>
2022-03-15 08:50:05 +0700

    fig bug quangminh

--- src/main/java/get/high/controller/FriendshipController.java
+++ src/main/java/get/high/controller/FriendshipController.java
@@ -1,11 +1,34 @@
 package get.high.controller;
 
+import get.high.model.entity.Friendship;
+import get.high.service.IFriendshipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @CrossOrigin("*")
 @RequestMapping("/friendship")
 public class FriendshipController {
+
+    @Autowired
+    private IFriendshipService friendshipService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Friendship>> showAll() {
+        Iterable<Friendship> friendships = friendshipService.findAll();
+
+        if (!friendships.iterator().hasNext()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(friendships, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/{from_user_id}/{to_user_id}")
+    public ResponseEntity<Friendship> addFriend(@PathVariable("from_user_id") long from_user_id, @PathVariable("to_user_id") long to_user_id) {
+
+    }
 }
