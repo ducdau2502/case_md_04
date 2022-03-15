1ffd9c58c8e60091799d114993980480e12c22b1
minhquang3002 <quangminhnguyen3002@gmail.com>
2022-03-15 09:03:29 +0700

    add post

--- src/main/java/get/high/service/ICommentService.java
+++ src/main/java/get/high/service/ICommentService.java
@@ -1,10 +1,7 @@
 package get.high.service;
 
 import get.high.model.entity.Comment;
-import get.high.model.entity.Post;
 
 public interface ICommentService extends IGeneralService<Comment> {
     Iterable<Comment> findAllByPost_Id(Long id);
-
-    void deleteAllByPost(Post post);
 }
